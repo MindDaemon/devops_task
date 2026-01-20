bf051e1254419456e01b8cab56ed1b2472592e70
litcoin-abuser <49125072+litcoin-abuser@users.noreply.github.com>
2026-01-20 18:31:44 +0100

    feat: login

--- starter.py
+++ starter.py
@@ -1,8 +1,16 @@
-def main():
+def login() -> bool:
+    username : str = input('Benutzer:')
+    return 'passwort' == input('Passwort:')
+
+def main() -> None:
     try:
-        print("Hello, World!")
+        print('Hallo Welt! Kannst du mich hören? Du darfst mich nicht beim Chatten stören. Mein Tagesablauf ist sehr klein, denn ich bin, bin, bi-bi-bin durchgehend online.')
+        if login() :
+            print('Erfolgreich angemeldet.')
+        else :
+            print('Nicht erfolgreich angemeldet.')
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f'An error occurred: {e}')
 
 if __name__ == "__main__":
     main()
